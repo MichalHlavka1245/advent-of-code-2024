3def3ddc8cedbbd6aee4f7715064d200541514f0
MichalHlavka1245 <150445003+MichalHlavka1245@users.noreply.github.com>
2024-12-08 12:12:18 +0100

    day 3-2

---
+++ adventofcode/day 3-2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(){
+    string text, total="";
+    ifstream inputFile("../data.txt");
+
+    if (!inputFile) {
+        cerr << "Error opening file data.txt" << endl;
+        return 1;
+    }
+
+    while(getline(inputFile, text)) total += text;
+
+    regex vzor(R"(mul\((\d{1,3}),(\d{1,3})\))"), vzorSwitch(R"(do\(\)|don't\(\))");
+    smatch match;
+    unsigned long long result = 0;
+    bool isOn = true;
+
+    vector<size_t> state;
+    sregex_iterator iterSwitch(total.begin(), total.end(), vzorSwitch), endSwitch;
+
+    while(iterSwitch != endSwitch){
+        smatch match = *iterSwitch;
+        if(match.str() == "don't()" && state.size() % 2 == 0) state.push_back(match.position());
+        else if(match.str() == "do()" && state.size() % 2 == 1) state.push_back(match.position());
+        ++iterSwitch;
+    }
+
+    sregex_iterator iter(total.begin(), total.end(), vzor), end;
+
+    while (iter != end) {
+        smatch match = *iter;
+        bool isOn = true;
+        size_t pos = match.position();
+
+        for(int i = 1; i < state.size(); i++){
+            if(pos>state[i-1]&&pos<state[i]){
+                (i%2==1?isOn=0:isOn=1);
+                break;
+            }
+        }
+
+        if(!state.empty()&&pos>state[state.size()-1]&&state.size()%2==1) isOn = false;
+
+        if(isOn){
+            int first = stoi(match[1]);
+            int second = stoi(match[2]);
+
+            result += first * second;
+            cout<<result<<endl;
+        }
+        ++iter;
+    }
+    cout << result << "\n";
+    return 0;
+}
