5fddca3d431af978367ee6b921187bebda05f1ff
Michal Hlávka <150445003+MichalHlavka1245@users.noreply.github.com>
2024-12-31 09:42:57 +0100

    Update day 1-2.cpp

--- adventofcode/day 1-2.cpp
+++ adventofcode/day 1-2.cpp
@@ -2,8 +2,7 @@
 #include <vector>
 
 using namespace std;
-int main()
-{
+int main(){
     vector<int> zoznam(1000),zoznam1(1000);
     int spolu=0;
     for (int c = 0; c < 1000; c++)
@@ -13,21 +12,16 @@ int main()
     }
     for (int i = 0; i < 1000; i++){
         int aktualne_cislo=zoznam[i];
-
-
-
         for (int j = 0; j < 1000; j++){
             int pocet=0;
             if(aktualne_cislo==zoznam1[j]){
                 pocet++;
                 if(pocet==0) spolu+=0;
-                else{
-                    spolu+=aktualne_cislo*pocet;
-                    cout<<spolu<<endl;
+                    else{
+                        spolu+=aktualne_cislo*pocet;
+                        cout<<spolu<<endl;
                 }
             }
-
-
         }
     }
     cout<<spolu<<endl;
