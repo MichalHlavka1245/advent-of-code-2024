3bb8e576c6cadb05de306ee79389e9c7a6e68116
Michal Hlávka <150445003+MichalHlavka1245@users.noreply.github.com>
2024-12-31 10:34:09 +0100

    Update day 2-2.cpp

--- adventofcode/day 2-2.cpp
+++ adventofcode/day 2-2.cpp
@@ -30,19 +30,15 @@ bool je_mozne_spravit_bezpecny(vector<int> riadok, int& pozicia_vymazaneho_cisla
     for (int i = 0; i < riadok.size(); i++) {
         vector<int> test_riadok = riadok;
         test_riadok.erase(test_riadok.begin() + i);
-
-
-        if (test_riadok.size() < 2) continue;
-
-
+        
+    if (test_riadok.size() < 2) continue;
         if (test_riadok[0] < test_riadok[1]) {
             if (scitanie(test_riadok)) {
                 pozicia_vymazaneho_cisla = i;
                 return true;
             }
         }
-
-        if (test_riadok[0] > test_riadok[1]) {
+    if (test_riadok[0] > test_riadok[1]) {
             if (odcitanie(test_riadok)) {
                 pozicia_vymazaneho_cisla = i;
                 return true;
@@ -57,31 +53,19 @@ int main() {
     string riadok_text;
 
     for (int i = 0; i < 1000; i++) {
-
-        if (!getline(cin, riadok_text)) break;
-
-
-        istringstream iss(riadok_text);
-
-
-        vector<int> riadok;
-        int cislo;
-
-
-        while (iss >> cislo) {
+    if (!getline(cin, riadok_text)) break;
+    istringstream iss(riadok_text);
+    vector<int> riadok;
+    int cislo;
+     while (iss >> cislo) {
             riadok.push_back(cislo);
         }
+//cout << "Pocet cisel v riadku " << i+1 << ": " << riadok.size() << endl;
 
-
-        cout << "Pocet cisel v riadku " << i+1 << ": " << riadok.size() << endl;
-
-
-        if (riadok.size() < 2) continue;
-
-
-        int pozicia_vymazaneho_cisla = -1;
-
-        bool povodny_bezpecny = false;
+if (riadok.size() < 2) continue;
+int pozicia_vymazaneho_cisla = -1;
+bool povodny_bezpecny = false;
+        
         if (riadok[0] < riadok[1]) {
             if (scitanie(riadok)) {
                 pocet_bezpecne++;
@@ -96,15 +80,10 @@ int main() {
             }
         }
 
-
-        if (!povodny_bezpecny) {
+if (!povodny_bezpecny) {
             if (je_mozne_spravit_bezpecny(riadok, pozicia_vymazaneho_cisla)) {
-
-                pocet_bezpecne++;
-
-
-                cout << "Riadok sa stal bezpecny po odstraneni cisla na pozicii "
-                     << pozicia_vymazaneho_cisla << endl;
+            pocet_bezpecne++;
+cout << "Riadok sa stal bezpecny po odstraneni cisla na pozicii "<< pozicia_vymazaneho_cisla << endl;
             }
         }
     }
