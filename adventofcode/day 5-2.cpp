dc6795a2a7ade1c9aa3badbd1edea96b010c2071
Michal Hlávka <150445003+MichalHlavka1245@users.noreply.github.com>
2025-01-04 08:21:27 +0100

    Update day 5-2.cpp

--- adventofcode/day 5-2.cpp
+++ adventofcode/day 5-2.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
 int main() {
     string input = "../input.txt";
     ifstream file(input);
     string line;
     long long vysledok = 0;
-    unordered_map<int, vector<int>> pravidla;
+    map<int, vector<int>> pravidla;
     vector<vector<int>> aktualizacie;
     while(getline(file, line)) {
         if (line.empty()) break;
@@ -13,16 +15,16 @@ int main() {
         pravidla[stoi(line.substr(0, idx))].push_back(stoi(line.substr(idx + 1, line.size())));
     }
     while(getline(file, line)) {
-        size_t current = 0;
-        size_t comma_idx = line.find(',');
+        int current = 0;
+        int comma_idx = line.find(',');
         aktualizacie.emplace_back();
         auto& update = aktualizacie.back();
         while(comma_idx != string::npos) {
-            update.push_back(stoi(line.substr(current, comma_idx - current)));
+            update.push_back(stoi(line.substr(current, comma_idx)));
             current = comma_idx + 1;
             comma_idx = line.find(',', current);
         }
-        update.push_back(stoi(line.substr(current, line.size() - current)));
+        update.push_back(stoi(line.substr(current, line.size())));
     }
     for (auto& update : aktualizacie) {
         bool follows_rules = true;
