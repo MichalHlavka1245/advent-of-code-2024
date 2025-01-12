b225bbad8f92eabb0d0cd15e2b8856a6fdb194fc
Michal Hlávka - ssnd <150445003+MichalHlavka1245@users.noreply.github.com>
2025-01-12 14:04:03 +0100

    Update day_12-1.cpp

--- adventofcode/day_12-1.cpp
+++ adventofcode/day_12-1.cpp
@@ -1,84 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct Point {
-    int row;
-    int col;
-    bool operator == (const Point& p) const {
-        return row == p.row && col == p.col;
-    }
+using Point = pair<int, int>;
+const array<Point, 4> motions{
+    Point(0, 1), Point(1, 0), Point(0, -1), Point(-1, 0)
 };
 
-constexpr array motions {
-    Point( 0, 1),
-    Point( 1, 0),
-    Point( 0,-1),
-    Point(-1, 0)
-};
-
-long long calculate_cost(
-    const int row_idx,
-    const int col_idx,
-    const vector<string>& mapa,
-    vector<vector<bool>>& visited) {
-    const auto c = mapa[row_idx][col_idx];  //rastliny
+long long calculate_cost(int row_idx, int col_idx, const vector<string>& mapa, vector<vector<bool>>& visited) {
+    const auto c = mapa[row_idx][col_idx];  // Rastliny
     if (c == '#') return 0;
-    const auto start = Point(row_idx, col_idx);
+    const Point start(row_idx, col_idx);
     long long cost = 0;
     queue<Point> q;
     q.push(start);
     long long area = 0;
     long long perimeter = 0;
-    while(!q.empty()) {
+
+    while (!q.empty()) {
         auto p = q.front();
         q.pop();
-        if (visited[p.row][p.col]) continue;
-        visited[p.row][p.col] = true;
+        if (visited[p.first][p.second]) continue;
+        visited[p.first][p.second] = true;
         area++;
+
         for (const auto& motion : motions) {
-            const auto next = Point(p.row + motion.row, p.col + motion.col);
-            if (mapa[next.row][next.col] != c) {
+            const Point next(p.first + motion.first, p.second + motion.second);
+            if (mapa[next.first][next.second] != c) {
                 perimeter++;
                 continue;
             }
-            if (mapa[next.row][next.col] == c && !visited[next.row][next.col]) {
+            if (!visited[next.first][next.second]) {
                 q.push(next);
             }
         }
     }
-    // std::cout << c << ": \nArea: " << area << "\nPerimeter: "<< perimeter << '\n';
+
     return area * perimeter;
 }
 
 int main() {
-    string input = "../input.txt";
-
-
-    ifstream file(input);
+    ifstream file("../input.txt");
     string line;
     vector<string> mapa;
-    mapa.emplace_back();
 
-    // Add a buffer
-    while(getline(file, line)) {
-        line = '#' + line + '#';
+    while (getline(file, line)) {
+        line = "#" + line + "#";
         mapa.push_back(line);
     }
-    mapa[0] = string(mapa[1].size(), '#');
-    mapa.emplace_back(mapa[0]);
 
-    vector<vector<bool>> visited (mapa.size(), vector<bool>(mapa[0].size(), false));
+   
+    string border(mapa[0].size(), '#');
+    mapa.insert(mapa.begin(), border);
+    mapa.push_back(border);
+
+    vector<vector<bool>> visited(mapa.size(), vector<bool>(mapa[0].size(), false));
     long long total_cost = 0;
-    for (const auto& [row_idx, row] : views::enumerate(mapa)) {
-        for (const auto& [col_idx, ele] : views::enumerate(row)) {
+
+    for (int row_idx = 0; row_idx < mapa.size(); ++row_idx) {
+        for (int col_idx = 0; col_idx < mapa[row_idx].size(); ++col_idx) {
             if (visited[row_idx][col_idx]) continue;
             const auto c = mapa[row_idx][col_idx];
             if (c == '#') continue;
             total_cost += calculate_cost(row_idx, col_idx, mapa, visited);
         }
     }
+
     cout << total_cost << '\n';
     return 0;
 }
-
-
 
