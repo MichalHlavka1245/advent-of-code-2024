bc611a58f68d7255e40c6eb7ed9bf79d68525ad1
Michal Hlávka - ssnd <150445003+MichalHlavka1245@users.noreply.github.com>
2025-01-13 10:58:42 +0100

    Update day_13-1.cpp

--- adventofcode/day_13-1.cpp
+++ adventofcode/day_13-1.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 tuple<bool, int, int> check_linear_combination(const int ax, const int ay, const int bx, const int by, const int cx, const int cy) {
-    if ((ax * by) == (bx * ay)) exit(0);
-    const int n1 = (cx * by - cy * bx) / (ax * by - ay * bx);
-    const int n2 = (cx * ay - cy * ax) / (bx * ay - by * ax);
+    if ((ax * by) == (bx * ay)) exit(0);;
+    const int A = (cx * by - cy * bx) / (ax * by - ay * bx);
+    const int B = (cx * ay - cy * ax) / (bx * ay - by * ax);
 
-    if (n1 * ax + n2 * bx != cx) return {false, -1, -1};
-    if (n1 * ay + n2 * by != cy) return {false, -1, -1};
-    if (n1 < 0 || n2 < 0 || n1 > 100 || n2 > 100) return {false, -1, -1};
-    return {true, n1, n2};
+    if (A * ax + B * bx != cx) return {false, -1, -1};
+    if (A * ay + B * by != cy) return {false, -1, -1};
+    if (A < 0 || B < 0 || A > 100 || B > 100) return {false, -1, -1};
+    return {true, A, B};
 }
 
 int main() {
@@ -34,9 +34,9 @@ int main() {
         regex_search(line, match, vzor);
         const int px = stoi(match[1]);
         const int py = stoi(match[2]);
-        const auto [possible, n1, n2] = check_linear_combination(ax, ay, bx, by, px, py);
+        const auto [possible, A, B] = check_linear_combination(ax, ay, bx, by, px, py);
 
-        if (possible) total_cost += (n1 * 3 + n2);
+        if (possible) total_cost += (A * 3 + B);
         if(!getline(file, line)) break;
     }
     cout << total_cost << '\n';
